40ab11aa0e0ef326d048cd459bc4ab4b5b6ca7c3
JayRajM97 <jayraj.mka@gmail.com>
2018-07-18 23:10:56 +0530

    Created First APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloApiView(APIView):
+    """Test APIView (HelloApiView)"""
+
+    def get(self, request, format=None):
+        """Retuens a list of APIView Features"""
+
+        an_apiview = [
+            'Tony Stark',
+            'Dr. Stephen Strange',
+            'Thor'
+        ]
+
+        return Response({'message': 'Avengers', 'an_apiview': an_apiview})
