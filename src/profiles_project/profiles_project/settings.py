3a2e6ee0d56bc3c3b32d0bcbc0e8ce40f4c9aee4
JayRajM97 <jayraj.mka@gmail.com>
2018-08-17 07:34:20 +0530

    Added AWS Server Allowed Host 1.

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'wm9yfk(+$@uwz3sxi%bk=gw-!9gmll60u)9@qqy$9c2ky^-shb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'ec2-184-72-110-235.compute-1.amazonaws.com
-']
+ALLOWED_HOSTS = ['ec2-184-72-110-235.compute-1.amazonaws.com']
 
 
 # Application definition
